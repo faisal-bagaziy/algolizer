29b7dc58f78ed3018602d5a1d6c2af0c63228473
faisalBagaziy <faisalbagaziy@gmail.com>
2023-04-28 05:59:31 +0300

    added this folder

---
+++ src/pages/algorithms-catagory/sorting.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import HeaderForRest from '../../../components/header-for-rest-section/header-for-rest-section'
+import ContentSortingAlgorithms from '../../../components/content-sorting-algorithms/content-sorting-algorithms'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Sorting() {
+  return (
+    <>
+        <HeaderForRest />
+        <ContentSortingAlgorithms />
+    </>
+  )
+}

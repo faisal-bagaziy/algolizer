d2249bdc6270f7d3263b0128cf779e3c06ae83b9
faisalBagaziy <faisalbagaziy@gmail.com>
2023-04-29 01:30:10 +0300

    added and fixed links too sorting algorithms

--- components/content-sorting-algorithms/content-sorting-algorithms.jsx
+++ components/content-sorting-algorithms/content-sorting-algorithms.jsx
@@ -11,13 +11,13 @@ function ContentSortingAlgorithms() {
                      <Link href="../sorting-algorithms/bubblesort">
                         <Card title="Bubble Sort" description="Repeatedly steps through the list to be sorted, compares each pair of adjacent items and swaps them if they are in the wrong order." icon="/sort.svg" width={100} height={100} />
                     </Link>
-                    <Link href="/">
+                    <Link href="../sorting-algorithms/selectionsort">
                         <Card title="Selection Sort" description="Repeatedly finding the minimum element from the unsorted part of the list and swapping it with the first element of the unsorted part." icon="/sort.svg" width={100} height={100} />
                     </Link>
-                    <Link href="/">
+                    <Link href="../sorting-algorithms/insertionsort">
                         <Card title="Insertion Sort" description="Repeatedly taking an unsorted element and inserting it into its correct position in the sorted part of the list." icon="/sort.svg" width={100} height={100} />
                     </Link>
-                    <Link href="/">
+                    <Link href="../sorting-algorithms/mergesort">
                         <Card title="Merge Sort" description="dividing the input list into two halves, sorting each half recursively using Merge Sort, and then merging the two sorted halves back together." icon="/sort.svg" width={100} height={100} />
                     </Link>
                     <Link href="/">

63d9138b4f21af681868692e573360b61ecff4b0
faisalBagaziy <faisalbagaziy@gmail.com>
2023-04-28 06:55:44 +0300

    added searching and learning pages

---
+++ src/pages/algorithms-catagory/searching.js
@@ -0,0 +1,17 @@
+
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import HeaderForRest from '../../../components/header-for-rest-section/header-for-rest-section'
+import ContentSearchingAlgorithms from '../../../components/content-searching-algorithms/content-searching-algorithms'
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Searching() {
+  return (
+    <>
+        <HeaderForRest />
+        <ContentSearchingAlgorithms />
+    </>
+  )
+}

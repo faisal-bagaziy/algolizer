b52470742d26a99505842e1d5ed10dc9a006f73f
faisalBagaziy <faisalbagaziy@gmail.com>
2023-05-04 15:11:17 +0300

    reworked the footer section

--- components/footer-section/footer-section.jsx
+++ components/footer-section/footer-section.jsx
@@ -6,35 +6,11 @@ function Footer() {
       <div className={styles.left}>
         <h2 className={styles.title}>Algolizer</h2>
         <p className={styles.description}>
-          this is a powerful tool to learn algorithms.<br/>
-          compatible with different type of devices.
+          This is a powerful tool to learn algorithms.<br/><br/>
         </p>
         <p className={styles.description}>
-            ©2023 - Algolizer  |   All right reserved
+            ©2023 - Algolizer  
         </p>
-      </div>
-      <div className={styles.right}>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>Products</h3>
-          <ul className={styles.sectionList}>
-            <li className={styles.sectionItem}>How it works</li>
-            <li className={styles.sectionItem}>Features</li>
-            <li className={styles.sectionItem}>Algorithms</li>
-          </ul>
-        </div>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>About</h3>
-          <ul className={styles.sectionList}>
-            <li className={styles.sectionItem}>About Us</li>
-          </ul>
-        </div>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>Contact</h3>
-          <ul className={styles.sectionList}>
-            <li className={styles.sectionItem}>Algolizer@gmail.com</li>
-
-          </ul>
-        </div>
       </div>
     </div>
   );

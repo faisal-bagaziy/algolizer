352498ca6a441512e73259a0985396bf085723a9
faisalBagaziy <faisalbagaziy@gmail.com>
2023-05-05 08:56:14 +0300

    fixed typos

--- components/content-whyus-section/content-whyus-section.jsx
+++ components/content-whyus-section/content-whyus-section.jsx
@@ -8,8 +8,8 @@ function ContentWhyUsSection() {
         <h1 className={styles.title}>The most complete algorithm visualizer around</h1>
         <div className={styles.cardsContainer}>
           <Card title="Great for novice" description="Algolizer is great for novice users who just started learning how to code and want to expand their knowledge about algorithms." icon="/egg.svg" width={75} height={75}/>
-          <Card title="Fast setup" description="Set up and start visualization within minutes.." icon="/launch.svg" width={75} height={75} />
-          <Card title="Great number of algorithms" description="Algolizer cover variety of important basic algorithms.." icon="/infinity.svg" width={75} height={75} />
+          <Card title="Fast setup" description="Set up and start visualization within minutes." icon="/launch.svg" width={75} height={75} />
+          <Card title="Great number of algorithms" description="Algolizer cover variety of important basic algorithms." icon="/infinity.svg" width={75} height={75} />
         </div>
       </div>
     </section>
